4f7bd7c2c8dd92ae8f80d31a750198b1de814640
nikol899 <nikol.899@gmail.com>
2019-05-09 15:06:42 -0400

    started some functions and divs

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 "use strict";
+// $(window).load(function(){
+//     //Disply the modal popup
+//       ('#myModal').modal('show');
+//   });
+
+
+
+
+// let roundStart = 1;
+// let userWin = 0;
+// let timerCount = 900;
+// let deckOfcards=[];
+// // let faceUp=[];
+// // let  faceDown=[];
+// let userChoice=[];
+// let userName= undefined;
+
+// cardGrid();
+
+
+//     const grid=document.querySelector("section.card_container");
+//     for (let i = 0; i < 3; i++) { // creates a row  of 2 .
+//         let row = document.createElement("div"); 
+//         row.classList.add("row");
+//         grid.append("div");
+//         for (let a = 0; a < 3; a++) { 
+//         let cell = document.createElement("div"); // builds a div
+//         cell.classList.add("cell"); //  stores cells in a variable classList and adds it to cells?
+//         row.append(cell); 
+// }
+//     }
+
+// function numberRound(){
+//     document.querySelector(".start_btn").innerText =  `Round: ${roundStart}`;
+// }
+    // document.querySelector(".start_btn").addEventListener("click", startGame);
+    // function startGame(){ 
+      
+    
+
+//  function displayCards(){
+//     document.querySelector(".card_container")
+//     let grid.innerHTML ={
+   
+//    deckOfcards = "hip-hop-playing-cards-1998-deck.jpg">};
+//     document.querySelector(".card_container").append(img);
+
+
+
+
+
+
+
+
+
+
+// let cards = document.querySelectorAll(".card_container"); 
+// if(i =0 ; i< cards.lenghts; i++){
+//     cards[1].addEventListener("click",displayCards); // 
+// 

ec5832e3da445e9ee7b628ab7a2b94c8ba529739
Brittany Hardy <behardy25@gmail.com>
2019-05-09 13:04:55 -0400

    photos

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+$('.stack').click(function () {
+
+    $(".card").each(function (e) {
+
+        setTimeout(function () {
+            $(".card").eq(e).attr("class", "card");
+        }, e * 150)
+
+    });
+
+});
+
+$('.spread').click(function () {
+
+    $(".card").each(function (e) {
+
+        setTimeout(function () {
+            $(".card").eq(e).attr("class", "card ani" + e);
+        }, e * 150)
+
+    });
+
+});

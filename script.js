b6325cf48f6a22ad0defac9195329c67cc5a8eb1
nikol899 <nikol.899@gmail.com>
2019-05-10 13:54:44 -0400

    shuffle add button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 "use strict";
+let roundNumber =1;
+let flippedCards=[]; // keep track of flipped cards in the array
+let count =0; // keeps count of cards total of 12 in a deck
+let cardsArray=[];
+
 const cards = document.querySelectorAll('.card');
 
 function flipCard() {
@@ -7,135 +12,37 @@ function flipCard() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cardArry = [
-//     {
-//         Name: "Sonya",
-//         img: "MK-headShots/SONIYAMK1.gif"
-//     },
-//     {
-//         Name: "Sonya",
-//         img: "MK-headShots/Sonya_MK_11.png"
-//     },
-
-//     {
-//         Name: "Jade",
-//         img: "MK-headShots/MK3-Jade-r1.gif"
-//     },
-//     {
-//         Name: "Jade",
-//         img: "MK-headShots/Jade_MK11.png"
-//     },
-//     {
-//         Name: "Kitana",
-//         img: "MK-headShots/Kitana_mk11.octet-stream.png"
-//     },
-//     {
-//         Name: "Kitana",
-//         img: "MK-headShots/Kitana_UMK3.gif"
-//     },
-//     {
-//         Name: "Sindel",
-//         img: "MK- headShots / Sindelrenderbywildboyz4.png"
-//     },
-//     {
-//         Name: "Sindel",
-//         img: "MK-headShots/Sindel-r.gif"
-//     },
-//     {
-//         Name: "Mileena",
-//         img: "MK-headShots/Millena-mk2-fix.gif"
-//     },
-//     {
-//         Name: "Mileena",
-//         img: "MK-headShots/Mileena_Render_PNG_3.png"
-//     },
-//     {
-//         Name: "Chun-li",
-//         img: "MK-headShots/Chun_Li.png"
-//     },
-//     {
-//         Name: "Chun-li",
-//         img: "MK-headShots/Chun_Li_1.gif"
-//     },
-
-
-
-// ]
-
-
-
-
-// const create = document.createElement("div");
-// // const card = document.createElement('div')
-
-// create.classList.add("card");
-// //     card.classList.add('card')
-// create.dataset.name = item.name;
-
-// //     card.dataset.name = item.name
-
-// create.style.backgeoundImage = `url(${item.img})`;
-//     card.style.backgroundImage = `url(${item.img})`
-
-//     // Append the div to the grid section
-//     grid.appendChild(card)
-
-
-// const game = document.getElementById('MK_Memory')
-
-// cardsArray.forEach(item => {
-
-//     const card = document.createElement('div')
-
-
-//     card.classList.add('card')
-
-//     card.dataset.name = item.name
-
-
-//     card.style.backgroundImage = `url(${item.img})`
-
-
-//     grid.appendChild(card)
-// })
+function setRoundNumber() {
+    document.querySelector(".startBtn").innerText=`Round : ${roundNumber}`;
+}
+
+function addsEventsElements(){
+    document.querySelector("main").addEventListener("click",function(event){
+        if(event.target.classList.contains("startBtn")){
+            startGame();
+            console.log(event);
+        }else if(event.target.classList.contains("resetBtn"));{
+            // resets the board and shuffle cards
+        }
+    }
+    )};
+function cardShuffle(){
+    var currentIndex = cardsArray.length, tempCards, randomCards;
+    while (currentIndex !== 0) {
+        randomCards = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        tempCards = cardsArray[currentIndex];
+        cardsArray[currentIndex] = cardsArray[randomCards];
+        cardsArray[randomCards] = tempCards;
+    }
+
+    return  cardsArray;
+};
+
+    function startGame(){
+    cardArray.sort(() => 0.5 - Math.random()); // shuffles the cards
+        if(firstChildElement[0].value === cardsArray.firstChildElement[1].value){
+            
+
+        }
+    }

78e4eef1e5fc05672d413d50bcf0ffee7af75148
arjun <arjun92homebiz@gmail.com>
2019-05-10 13:23:46 -0400

    startbutton

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 "use strict";
-const cards = document.querySelectorAll('.card');
+let roundNumber =1;
+let flippedCards=[]; // keep track of flipped cards in the array
+let count =0; // keeps count of cards total of 12 in a deck
 
+let cards = document.querySelectorAll('.card');
 function flipCard() {
     this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// let startBtn = document.getElementsByClassName("startBtn");
+
+function setRoundNumber() {
+    document.querySelector(".startBtn").innerText=`Round : ${roundNumber}`;
+}
+function countDown(){
+  
+
+ }
+function addsEventsElements(){
+    document.querySelector("main").addEventListener("click",function(event){
+        if(event.target.classList.contains("startBtn")){
+            
+            startGame();
+            console.log(event);
+        }else if(event.target.classList.contains("resetBtn"));{
+            // resets the board and shuffle cards
+        }
+    }
+    )};
+    function startGame(){
+    cardArray.sort(() => 0.5 - Math.random()) // shuffles the cards
+    document.getElementById("timer").innerHTML = 90;
+    countDown();
+
+    }

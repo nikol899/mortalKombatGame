8711f4074c39210f1a853b17fdced1c40c8ca84a
nikol899 <nikol.899@gmail.com>
2019-05-10 19:54:53 -0400

    some functions

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 "use strict";
 let roundNumber =1;
-let flippedCards=[]; // keep track of flipped cards in the array
-let count =0; // keeps count of cards total of 12 in a deck
-let cardsArray=[];
+let flippedCards=[]; // keep track of open cards in the array
+let countMove =0; // keeps count of cards total of 12 in a deck
+
+
+// making div element into an array
+let nodes = document.querySelectorAll("div")
+const cardsArray = Array.from(nodes); // holds the node list as an array in the cards array
 
 const cards = document.querySelectorAll('.card');
-
 function flipCard() {
     this.classList.toggle('flip');
 }
-
 cards.forEach(card => card.addEventListener('click', flipCard));
+console.log(cards);
 
-function setRoundNumber() {
-    document.querySelector(".startBtn").innerText=`Round : ${roundNumber}`;
-}
 
-function addsEventsElements(){
-    document.querySelector("main").addEventListener("click",function(event){
-        if(event.target.classList.contains("startBtn")){
-            startGame();
-            console.log(event);
-        }else if(event.target.classList.contains("resetBtn"));{
-            // resets the board and shuffle cards
-        }
-    }
-    )};
-function cardShuffle(){
-    var currentIndex = cardsArray.length, tempCards, randomCards;
+document.querySelector(".startBtn").addEventListener("click",startGame);
+document.querySelector("main").addEventListener("click",cardShuffle) ; 
+
+function cardShuffle(array){
+    let currentIndex = cardsArray.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
-        randomCards = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        tempCards = cardsArray[currentIndex];
-        cardsArray[currentIndex] = cardsArray[randomCards];
-        cardsArray[randomCards] = tempCards;
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
     }
+    // return array;
+  }
 
-    return  cardsArray;
-};
+const newCards = document.querySelector(".cards") 
+function startGame(){
+    flippedCards =[];
 
-    function startGame(){
-    cardArray.sort(() => 0.5 - Math.random()); // shuffles the cards
-        if(firstChildElement[0].value === cardsArray.firstChildElement[1].value){
-            
 
-        }
+
+
+    cardShuffle();
+    let shuffledCards = cardShuffle(newCards);
+    for(var i= 0;i<shuffledCards.length; i++ ){
+        [].forEach.call(shuffledCards.length,function(item){
+            newCards.appendChild(item)
+        });
     }
+}
+startGame(); 

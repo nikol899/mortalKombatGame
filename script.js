8663f070a0cd97580b6218f979c73e2426493e32
arjun <arjun92homebiz@gmail.com>
2019-05-10 10:11:17 -0400

    made start button

--- script.js
+++ script.js
@@ -1,1 +1,115 @@
 "use strict";
+
+let roundStart = 1;
+let timeCount = 900;
+let user = undefined;
+let deckOfCards = [];
+let userChoice = [];
+let startBtn = document.getElementsByClassName("startBtn");
+
+document.querySelector(".startBtn").addEventListener("click", startGame)
+    function startGame(event) {
+        let timeCount = 900;
+        let card = document.getElementsByClassName("card");
+        for (var i = 0; i < cards.length; i++){
+        cards[i].addEventListener("click", displayCard);
+        let displayCard = function (){
+            this.classList.toggle("open");
+            this.classList.toggle("show");
+            this.classList.toggle("disabled");
+        }
+    }
+    }
+
+//cards array holds all cards
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+// loop to add event listeners to each card
+for (var i = 0; i < cards.length; i++){
+   cards[i].addEventListener("click", displayCard);
+};
+
+// toggles open and show class to display cards
+let displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+ }
+
+ // Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+// deck of all cards in game
+const deck = document.querySelector(".deck");
+function startGame(){
+   let shuffledCards = shuffle(cards);
+   for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
+}
+window.onload;
+
+//add opened cards to OpenedCards list and check if cards are match or not
+function cardOpen() {
+    openedCards.push(this);
+    let len = openedCards.length;
+    if(len === 2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
+
+//for when cards match
+function matched(){
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    openedCards = [];
+}
+
+//for when cards don't match
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        enable();
+        openedCards = [];
+    },1100);
+}
+
+//disable cards temporarily
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+//enable cards and disable matched cards
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}

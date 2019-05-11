a844e6eac1be6fd652f58ea0b7e588d19f73a3e8
nikol899 <nikol.899@gmail.com>
2019-05-11 11:14:35 -0400

    m

--- script.js
+++ script.js
@@ -1,49 +1,72 @@
 "use strict";
 let roundNumber =1;
 let flippedCards=[]; // keep track of flipped cards in the array
-let count =0; // keeps count of cards total of 12 in a deck
+let cardCount = 0; // count of cards open
 let cardsArray=[];
+let guessOne="";
+let guessTwo ="";
 
+
+
+
+
+// main.addEventListener("click", function(event){
+//     if(event.target.nodeName === "SECTION"){
+//     return
+// }
+//     event.target.classList.add("selected")
+// })
+//  // keeps 2 cards flipped
+ 
+// if (count <2){
+//     count ++;
+//     event.target.classList.add("selected")
+
+// }
+
+//  document.querySelector(".selected", matchCards);
+//     function matchCards(){
+//     selected.forEach(function(card)
+//     cards.classList.add("match");
+// });
+// };
 const cards = document.querySelectorAll('.card');
+console.log(cards);
+// function flipCard() {
+//     this.classList.toggle('flip');
+// }
 
-function flipCard() {
-    this.classList.toggle('flip');
-}
+// cards.forEach(card => card.addEventListener('click', flipCard));
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+// function setRoundNumber() {
+//     document.querySelector(".startBtn").innerText=`Round : ${roundNumber}`;
+// }
 
-function setRoundNumber() {
-    document.querySelector(".startBtn").innerText=`Round : ${roundNumber}`;
-}
+// // function addsEventsElements(){
+//     document.querySelector("main").addEventListener("click",function(event){
+//         if(event.target.classList.contains("startBtn")){
+//             startGame();
+//             console.log(event);
+//         }else if(event.target.classList.contains("resetBtn"));}
+//             // resets the board and shuffle cards
+//  }
+//     // }
+//     // )};
+// function cardShuffle(array){
+//     let currentIndex = cardsArray.length, tempCards, randomCards;
+//     while (currentIndex !== 0) {
+//         randomCards = Math.floor(Math.random() * currentIndex);t
+//         currentIndex -= 1;
+//         tempCards = cardsArray[currentIndex];
+//         cardsArray[currentIndex] = cardsArray[randomCards];
+//         cardsArray[randomCards] = tempCards;
+//     }
 
-function addsEventsElements(){
-    document.querySelector("main").addEventListener("click",function(event){
-        if(event.target.classList.contains("startBtn")){
-            startGame();
-            console.log(event);
-        }else if(event.target.classList.contains("resetBtn"));{
-            // resets the board and shuffle cards
-        }
-    }
-    )};
-function cardShuffle(array){
-    let currentIndex = cardsArray.length, tempCards, randomCards;
-    while (currentIndex !== 0) {
-        randomCards = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        tempCards = cardsArray[currentIndex];
-        cardsArray[currentIndex] = cardsArray[randomCards];
-        cardsArray[randomCards] = tempCards;
-    }
+//     return  array;
+// };
 
-    return  array;
-};
-
-    function startGame(){
-        cardShuffle();
-        if(firstChildElement[0].value === cardsArray.firstChildElement[1].value){
-            
-
-        }
-    }
+//     function startGame(){
+//         cardShuffle();
+   
+//         }
     

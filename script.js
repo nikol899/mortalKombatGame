be92675f56f25bab5d42e51368c7c3aa908a49b7
nikol899 <nikol.899@gmail.com>
2019-05-11 18:14:07 -0400

    corrected matching still needs work

--- script.js
+++ script.js
@@ -28,47 +28,59 @@ let card = document.getElementsByClassName("card");
 	for( let i =0;i<cards.length; i++){
 	 
     }
+// deck of all cards
+const deck = document.getElementById("card-deck");
+// counts # of moves
+let moves =0;
+counter=document.querySelector(".moves");
 
-if(count < 2){
-    count ++;
-    event.target.classList.add("flippedCards");
+let matchedCards=document.getElementsByClassName("match");
+
+document.body.onload = startGame();
+
+function startGame(){
+    
 }
+// if(count < 2){
+//     count ++;
+//     event.target.classList.add("flippedCards");
+// }
 
 
-function matchedCards(){
-    let flippedCards = document.querySelectorAll(".card")
-    flippedCards.forEach(card =>{
-        card.classList.add("match")
-    })
-}
+// function matchedCards(){
+//     let flippedCards = document.querySelectorAll(".card")
+//     flippedCards.forEach(card =>{
+//         card.classList.add("match")
+//     })
+// }
 
-function resetGuess(){
-    guessOne ="";
-    guessTwo="";
-    count =0;
-    flippedCards.forEach(function(card){
-        card.classList.remove(flippedCards);
-    });
-}
+// function resetGuess(){
+//     guessOne ="";
+//     guessTwo="";
+//     count =0;
+//     flippedCards.forEach(function(card){
+//         card.classList.remove(flippedCards);
+//     });
+// }
 
-cards.addEventListener("click",function(event){
-    if(count <2){
-        count ++;
-        if(count === 1){
-            guessOne = event.target.tagName.value
-            console.log(event);
-            event.target.classList.add(".card")
-        }else{
-            guessTwo = event.target.tagName.value
-            event.target.add(".card")
-        }
+// cards.addEventListener("click",function(event){
+//     if(count <2){
+//         count ++;
+//         if(count === 1){
+//             guessOne = event.target.tagName.value
+//             console.log(event);
+//             event.target.classList.add(".card")
+//         }else{
+//             guessTwo = event.target.tagName.value
+//             event.target.add(".card")
+//         }
 
-    if(guessOne && guessTwo ){
-        if(guessOne === guessTwo){
-            matchedCards();
-            resetGuess();
-        }
-    }
-} previousChoice=event.target;
+//     if(guessOne && guessTwo ){
+//         if(guessOne === guessTwo){
+//             matchedCards();
+//             resetGuess();
+//         }
+//     }
+// } previousChoice=event.target;
 
-})
+// })
--- script2.js
+++ script2.js
@@ -2,94 +2,105 @@ let card = document.getElementsByClassName("card");
 let cardz = [...card];
 let cardsMatched = document.getElementsByClassName("match");
 
+const deck = document.getElementById("card-deck"); 
 const cards = document.querySelectorAll('.card');
+let hasFlipped =  false;
+let guessOne, guessTwo;
 
+let lock=false; // locks board prevents cards from flipping before are match or hidden
 function flipCard() {
-    this.classList.toggle('flip');
+    if(lock)return;
+    if(this ===guessOne)return;
+
+    this.classList.add('flip');
+
+    if(!hasFlipped){
+        hasFlipped = true;
+        guessOne = this;
+        return;
+    }
+    guessTwo=this;
+    lock = true;
+
+    matchCheck();
 }
+function matchCheck(){
+    if(guessOne.dataset.framework === guessTwo.dataset.framework){
+        disable();
+        return;
+    }
+    unflipped();
+}
+function disable(){
+    guessOne.removeEventListener("click",flipCard);
+    guessTwo.removeEventListener("click",flipCard);
+    resetGame();
+}
+function unflipped(){
+     setTimeOut(() => {
+        guessOne.classList.remove("flip");
+        guessTwo.classList.remove("flip");
+        resetGame();
+    },9000)
+}
+function resetGame(){
+    [hasFlipped,lock]=[false,false];
+    [guessOne,guessTwo]=[null,null];
+}
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+        while(currentIndex !==0){
+            randomIndex = Math.floor(Math.random()* currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 console.log(cardz);
 
-const deck = document.getElementById("card-deck"); 
-
-let selectedCards = [];
 
 
+// let selectedCards = [];
 
-function shuffle(array) {
 
 
-    let currentIndex = array.length, temporaryValue, randomIndex;
+// function shuffle(array) {
 
-    while(currentIndex !==0){
-        randomIndex = Math.floor(Math.random()* currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-document.body.onload = startGame();
 
-function startGame(){
-    selectedCards=[]; // open cards array
+//     let currentIndex = array.length, temporaryValue, randomIndex;
 
-    cardz = shuffle(cardz);
+//     while(currentIndex !==0){
+//         randomIndex = Math.floor(Math.random()* currentIndex);
+//         currentIndex -= 1;
+//         temporaryValue = array[currentIndex];
+//         array[currentIndex] = array[randomIndex];
+//         array[randomIndex] = temporaryValue;
+//     }
+//     return array;
+// }
+// document.body.onload = startGame();
 
-    for (let i =0; i< 12; i++){
-        deck.innerHTML ="";
-        [].forEach.call(cardz,function(item) {
-            deck.appendChild(item);
+// function startGame(){
+//     selectedCards=[]; // open cards array
+
+//     cardz = shuffle(cardz);
+
+//     for (let i =0; i< 12; i++){
+//         deck.innerHTML ="";
+//         [].forEach.call(cardz,function(item) {
+//             deck.appendChild(item);
             
-        });
-    //    cardz[i].classList.remove("flip");
-       carz[i].classList.remove("show","open","disable");
-        }
-    }
-    function display(){
-        this.class.toggle("open");
-        this.class.toggle("show");
-        this.class.toggle("disable");
-    }
-
-    function openCardsMatch(){
-        selectedCards.push(this);
-        let move = selectedCards.length;
-        if(move === 2){
-            countMove();
-            if(selectedCards[0].value === selectedCards[1].value){
-            cardMatched();
-        }else{
-            notMatch();
-        }
-    }
-
-function cardMatched(){
-    selectedCards[0].classList.add("match","disable");
-    selectedCards[1].classList.add("match","disable");
-    selectedCards[0].classList.add("show","open","no-event");
-    selectedCards[1].classList.add("show","open","no-event");
-    selectedCards=[];
-}
-function notMatch(){
-    selectedCards[0].classList.add("nomatch");
-    selectedCards[1].classList.add("nomatch");
-    disable();
-    selectedCards[0].classList.remove("show", "open", "no-event","unmatched");
-    selectedCards[1].classList.remove("show", "open", "no-event","unmatched");
-    enable();
-}
-function disable(){
-    Array.prototype.filter.call(cards, function(cardz){
-        cardz.classList.add('disabled');
-    });
-}
-for (var i = 0; i < cardz.length; i++){
-    cardz = card[i];
-    cardz.addEventListener("click", display);
-    cardz.addEventListener("click", openCardMatch);
+//         });
+//        cardz[i].classList.remove("flip");
     
-};
-}
+//         }
+//     }
+
+
+
+
+
